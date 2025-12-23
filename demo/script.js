196d89184ac48f1be2ef34e1268d3bbac065ba2a
chlsuun <chlsuun@users.noreply.github.com>
2025-12-23 15:48:14 +0900

    wip: fixing issues

--- demo/script.js
+++ demo/script.js
@@ -222,10 +222,14 @@ function updateSaynoEmotion(emotion) {
     gameState.saynoEmotion = emotion;
     const emotionData = saynoEmotions[emotion];
 
-    // Update character visual
+    // Update character visual with ACTUAL IMAGE
     saynoCharacter.innerHTML = `
         <div class="character-image ${emotionData.class}">
-            <div class="character-emoji">${emotionData.emoji}</div>
+            <img src="${emotionData.image}" 
+                 alt="Sayno ${emotion}" 
+                 class="character-img" 
+                 onerror="this.style.display='none'; this.nextElementSibling.style.display='flex';">
+            <div class="character-emoji" style="display:none;">${emotionData.emoji}</div>
         </div>
     `;
 
